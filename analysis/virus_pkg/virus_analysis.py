0b2d6333900aab55ae130b3ea0bcb8af3a9f71b4
Sim SangHyun <ipod1597@naver.com>
2017-05-24 10:36:03 +0900

    upx check

--- analysis/virus_pkg/virus_analysis.py
+++ analysis/virus_pkg/virus_analysis.py
@@ -1,10 +1,8 @@
 from analysis.string_pkg import string
 from analysis.packing_pkg import packing
 from . import virus
-def analysis():
 
-    print(string.string_util.check_url())
-    print(string.string_util.check_ip())
+def analysis():
     print(packing.check_packing())
     print(string.string_util.check_url())
     print(string.string_util.check_ip())
